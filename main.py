dd9937c341ff312b9dce8f9b96206ea89df6716b
Ved Jaggi <ved@vedjaggi.com>
2026-02-25 23:12:37 +0800

    inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="MuVault",
+        description="Allows you to connect and pull music from a remote source",
+    )
+
+
+if __name__ == "__main__":
+    main()
